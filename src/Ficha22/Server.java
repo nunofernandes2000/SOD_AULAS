f1def7447dbf9663ee3323bd20c22fb6e9faa167
Necas2000 <21635@ipportalegre.pt>
2022-05-06 11:43:49 +0100

    **ModificaçõesV2**

--- src/Ficha22/Server.java
+++ src/Ficha22/Server.java
@@ -1,5 +1,3 @@
-//Validação do Porto(portNumber) --> [do 1º if até ao fim do 2º if]
-
 package Ficha22;
 
 import java.io.*;
@@ -13,139 +11,129 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Server {
 
-    private final static int SOCKET_TIMEOUT = 20000; // [20 segundos]
-    private final static int N_THREADS = 10; // Númerro máximo de Threads que pode utilizar no programa
-    private final static String PATH = System.getProperty("user.dir") + "\\src\\Ficha22\\raffle.txt"; //Variavel com o caminho até ao ficheiro pretendido
+    private final static int SOCKET_TIMEOUT = 10000;
+    private final static int N_THREADS = 10;
+    private final static String PATH = System.getProperty("user.dir") + "\\src\\Ficha22\\";
     private final static String RAFFLE_FILE = "raffle.txt";
     private final static String ITEMS_FILE = "items.txt";
-    private final static Lock itemsLock = new ReentrantLock(); //Praticamente server para criar um  semaforo mais simples, que é (0) ou (1)
-    private final static Lock raffleLock = new ReentrantLock(); //Praticamente server para criar um  semaforo mais simples, que é (0) ou (1)
+    private final static Lock itemsLock = new ReentrantLock();
+    private final static Lock raffleLock = new ReentrantLock();
 
     public static void main(String[] args) {
 
-
-
-        if (args.length != 1){
+        if (args.length != 1) {
             System.err.println("Para usar, execute java EchoServer \"número do porto\"");
             System.exit(1);
         }
 
         int portNumber = 0;
 
-
         try {
-            portNumber = Integer.parseInt(args[0]); //Verificar se o primeiro valor introduzido no array é um número inteiro
+            portNumber = Integer.parseInt(args[0]);
 
-        } catch (NumberFormatException e){
-            System.err.println("Porto inválido: " + args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Main: Porto inválido: " + args[0]);
             System.exit(2);
         }
 
-        if (portNumber < 1024 || portNumber > 65535){
-            System.err.println("Porto inválido: " + args[0]);
+        if (portNumber < 1024 || portNumber > 65535) {
+            System.err.println("Main: Porto inválido: " + args[0]);
             System.exit(3);
         }
 
         try
-                (ServerSocket serverSocket = new ServerSocket(portNumber))
+                (
+                        ServerSocket serverSocket = new ServerSocket(portNumber)
+                )
         {
+            Socket clientSocket = new Socket();
 
+           serverSocket.setSoTimeout(SOCKET_TIMEOUT);
 
-            Socket clientSocket = new Socket(); //Criar um novo Socket
-
-
-            serverSocket.setSoTimeout(SOCKET_TIMEOUT); //Ao fim do número inserido 20 segundos neste caso, que ele está parado, gera uma exeção e sai
-
-            ExecutorService executor = Executors.newFixedThreadPool(N_THREADS); //Esta linha codigo, cria uma pool de threads ou seja só utiliza o número definido que neste caso é N_THREAD = 10
+            ExecutorService executor = Executors.newFixedThreadPool(N_THREADS);
 
             createRaffleFile();
 
-            while (true){
-
+            while (true) {
 
                 System.out.println("Main: À espera de novas ligações");
 
                 try {
-
-                    clientSocket = serverSocket.accept(); //Aguarda novas ligações após receber qualquer ligação
+                    clientSocket = serverSocket.accept();
                     System.out.println("Main: Nova ligação");
 
-                } catch (SocketTimeoutException e){
+                } catch (SocketTimeoutException e) {
                     System.out.println("Main: Acabou o tempo de sorteio");
                     executor.shutdownNow();
                     break;
 
-
-                } catch (IOException e){
-
-                    System.out.println("Main: Ocorreu um erro de I/0 ao tentar criar o socket no porto " + portNumber);
+                } catch (IOException e) {
+                    System.err.println("Main: Ocorreu um erro de I/O ao tentar criar o socket no porto " + portNumber);
                     System.exit(4);
-
                 }
 
-                executor.execute(new ServerThread(clientSocket, PATH + ITEMS_FILE, PATH + RAFFLE_FILE, itemsLock, raffleLock));
+                executor.execute(new ServerThread(clientSocket, PATH + ITEMS_FILE,
+                        PATH + RAFFLE_FILE, itemsLock, raffleLock));
             }
 
-        } catch (IOException e){
-            System.out.println("Main: Ocorreu um erro de I/0 ao tentar criar o socket no porto " + portNumber);
+        } catch (IOException e) {
+            System.err.println("Main: Ocorreu um erro de I/O ao tentar criar o socket no porto " + portNumber);
             System.exit(5);
         }
-
-
     }
 
-    private static void createRaffleFile(){
+    private static void createRaffleFile() {
         BufferedReader fileItemsReader = null;
         BufferedWriter fileRaffleWriter = null;
 
         try {
             fileRaffleWriter = new BufferedWriter(new FileWriter(PATH + RAFFLE_FILE));
+
         } catch (IOException e) {
-            System.out.println("Main: Erro de criação do buffer de escrita para o ficheiro " + RAFFLE_FILE);
+            System.err.println("Main: Erro de criação do buffer de escrita para o ficheiro " + RAFFLE_FILE);
             System.exit(6);
         }
 
         try {
             fileItemsReader = new BufferedReader(new FileReader(PATH + ITEMS_FILE));
+
         } catch (FileNotFoundException e) {
-            System.out.println("Main: Erro de criação do buffer de leitura para o ficheiro " + ITEMS_FILE);
-            System.exit(7);
-        } finally {
+            System.err.println("Main: Erro de criação do buffer de leitura para o ficheiro " + ITEMS_FILE);
 
             try {
                 fileRaffleWriter.close();
 
-            } catch (IOException e){
-                System.out.println("Mian: Erro de fecho dos buffers do ficheiro " + RAFFLE_FILE);
-                System.exit(8);
-
+            } catch (IOException e2) {
+                System.err.println("Main: Erro de fecho dos buffers do ficheiro " + RAFFLE_FILE);
             }
+            System.exit(7);
         }
 
         String line = "";
 
-        while (line != null){
-
-            try {
+        try {
+            while (line != null) {
                 line = fileItemsReader.readLine();
 
-                fileRaffleWriter.write("0\n");
+                if (line != null) {
+                    fileRaffleWriter.write("0\n");
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Main: Erro de leitura dos ficheiros " + ITEMS_FILE + " ou " + RAFFLE_FILE);
+            System.exit(8);
+
+        } finally {
+            try {
+                fileItemsReader.close();
+                fileRaffleWriter.close();
 
             } catch (IOException e) {
-                System.out.println("Main: Erro de leitura do ficheiro " + ITEMS_FILE);
+                System.err.println("Main: Erro de fecho dos buffers dos ficheiros " + ITEMS_FILE +
+                        " e " + RAFFLE_FILE);
                 System.exit(9);
-            } finally {
-
-                try {
-                    fileItemsReader.close();
-                    fileRaffleWriter.close();
-                } catch (IOException e) {
-                    System.out.println("Mian: Erro de fchei dos buffers dos ficheiros " + ITEMS_FILE + " e " + RAFFLE_FILE);
-                    System.exit(10);
-                }
-
             }
         }
-
     }
 }
--- src/Ficha22/ServerThread.java
+++ src/Ficha22/ServerThread.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 
-public class ServerThread  extends Thread{
+public class ServerThread extends Thread {
 
     private Socket clientSocket;
     private String itemsFilePath;
@@ -14,13 +14,15 @@ public class ServerThread  extends Thread{
     private Lock itemsLock;
     private Lock raffleLock;
 
-    private BufferedReader fileItemsBReader; //Buffer que lê ficheiros dos items
-    private BufferedReader filesRaffleBReader; // Buffer que lê ficheiro do sorteio
-    private BufferedWriter filesRaffleBWriter; // Buffer que escreve no ficheiro do sorteio
+    private BufferedReader fileItemsBReader;
+    private BufferedReader fileRaffleBReader;
+    private BufferedWriter fileRaffleBWriter;
 
     private int itemNumberRaffle;
 
-    public ServerThread(Socket clientSocket, String itemsFilePath, String raffleFilePath, Lock itemsLock, Lock raffleLock) {
+    public ServerThread(Socket clientSocket, String itemsFilePath, String raffleFilePath,
+                        Lock itemsLock, Lock raffleLock) {
+
         this.clientSocket = clientSocket;
         this.itemsFilePath = itemsFilePath;
         this.raffleFilePath = raffleFilePath;
@@ -28,17 +30,13 @@ public class ServerThread  extends Thread{
         this.raffleLock = raffleLock;
     }
 
-
     @Override
-    public void run(){
-
+    public void run() {
         try (
-
-                PrintWriter out = new PrintWriter(this.clientSocket.getOutputStream(), true); // autoflush-> server para enviar sem nos precupamos em limpar
+                PrintWriter out = new PrintWriter(this.clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
         )
         {
-
             doRaffle();
 
             String option = "";
@@ -46,89 +44,87 @@ public class ServerThread  extends Thread{
             do {
                 option = in.readLine();
 
-                switch (option){
-
+                switch (option) {
                     case "1":
-                        System.out.println("Thread " + this.getName() + ": A enviar a lista de items");
+                        System.out.println("Thread " + this.getName() + ": A enviar a lista de itens");
                         sendItemsList(out);
                         break;
 
                     case "2":
-
-                        if (this.isInterrupted()){
+                        if (Thread.currentThread().isInterrupted()) {
                             System.out.println("Thread " + this.getName() + ": A enviar os resultados do sorteio");
-                            out.println(1); // Se o cliente receber 1, quer dizer vai receber os resultados logo a seguir
+                            out.println(1);
                             sendResults(out);
 
                         } else {
-                            System.out.println("Thread " + this.getName() + ": Resultados não enviados, pois o sorteio ainda não acabou");
-                            out.println(0); // Se o cliente receber 0, quer dizer que ainda não acabou, logo não vai receber os resultados
+                            System.out.println("Thread " + this.getName() + ": Resultados não enviados, pois o sorteio " +
+                                    "ainda não acabou");
+                            out.println(0);
                         }
-
                         break;
 
                     case "3":
-                        System.out.println("Thread " + this.getName() + ": O cliente saiu");
+                        System.out.println("Thread " + this.getName() + ": O cliente saíu");
                         break;
                 }
 
-
-            } while (!option.equals("3")); //Enquanto for a opção for diferente de 3 o programa continua, quando o cliente digitar o 3, o cliente vai sair do ciclo
-
+            } while (!option.equals("3"));
 
 
-        } catch (IOException e){
-            System.out.println("Thread " + this.getName() + ": Erro de criação dos buffers do socket");
+        } catch (IOException e) {
+            System.err.println("Thread " + this.getName() + ": Erro de criação dos buffers do socket");
             System.exit(1);
         }
     }
 
     private void doRaffle(){
 
-        List<String> raffleList = new ArrayList<>(); //Criaçao de um Array(neste caso uma ArrayList)
+        List<String> raffleList = new ArrayList<>();
 
         String line = "";
-        int counter = 0; //Contador inicializado a 0
+        int counter = 0;
 
-        this.raffleLock.lock(); // A primeira thread bloqueia o acesso ao codigo, das threads que chegam a seguir
+        this.raffleLock.lock();
 
         try {
+            this.fileRaffleBReader = new BufferedReader(new FileReader(this.raffleFilePath));
 
-            this.fileItemsBReader = new BufferedReader(new FileReader(this.raffleFilePath));
+            while (line != null) {
 
-            while (line != null){
+                line = this.fileRaffleBReader.readLine();
 
-                line = this.filesRaffleBReader.readLine();
-                raffleList.add(line);
+                if (line != null) {
+                    raffleList.add(line);
 
-                if (line.equals("0")){
-                    counter ++;
+                    if (line.equals("0")) {
+                        counter++;
+                    }
                 }
             }
 
-            this.filesRaffleBReader.close(); // Serve para fechar o Buffer de leitura do ficheiro de sorteio
+            this.fileRaffleBReader.close();
 
         } catch (FileNotFoundException e) {
-            System.out.println("Thread " + this.getName() + ": Ficheiros de sorteio não encontrado");
+            System.err.println("Thread " + this.getName() + ": Ficheiro de sorteio não encontrado");
             this.raffleLock.unlock();
             System.exit(2);
 
-        } catch (IOException e){
-            System.out.println("Thread " + this.getName() + ": Erro de leitura do ficheiro do sorteio");
+        } catch (IOException e) {
+            System.err.println("Thread " + this.getName() + ": Erro de leitura ou fecho do ficheiro de sorteio");
             this.raffleLock.unlock();
             System.exit(3);
-
         }
 
-        int number = (int) (Math.random() *  counter + 1); // Gera um numero entre 0 e o numero que estiver dentro do contador
+        int number = (int)(Math.random() * counter + 1);
 
         int zeroCounter = 0;
-        for (int i = 0; 1 < raffleList.size(); i++){
-            if (raffleList.get(i).equals("0")){
+        for (int i = 0; i < raffleList.size(); i++) {
+
+            if (raffleList.get(i).equals("0")) {
                 zeroCounter++;
             }
 
-            if (zeroCounter == number){
+            if (zeroCounter == number) {
                 raffleList.set(i,this.getName());
 
                 itemNumberRaffle = i;
@@ -138,87 +134,83 @@ public class ServerThread  extends Thread{
         }
 
         try {
-            this.filesRaffleBWriter = new BufferedWriter(new FileWriter(this.raffleFilePath));
+            this.fileRaffleBWriter = new BufferedWriter(new FileWriter(this.raffleFilePath));
 
-            for (int i = 0; i < raffleList.size() ; i++) {
-                this.filesRaffleBWriter.write(raffleList.get(i) + "\n");
+            for (int i = 0; i < raffleList.size(); i++) {
+                this.fileRaffleBWriter.write(raffleList.get(i) + "\n");
             }
 
-            this.filesRaffleBWriter.close();
-
+            this.fileRaffleBWriter.close();
 
         } catch (IOException e) {
-            System.out.println("Thread " + this.getName() + ": Erro de escrita,  abertura ou fecho do ficheiro de sorteio");
+            System.err.println("Thread " + this.getName() + ": Erro de escrita, abertura ou fecho do ficheiro de sorteio");
             this.raffleLock.unlock();
             System.exit(4);
         }
 
-        this.raffleLock.unlock(); // O Acesso é desbloqueado, depois da thread acabar de fazer o bloco de código
-
+        this.raffleLock.unlock();
     }
 
-    private void sendItemsList(PrintWriter out){
+    private void sendItemsList(PrintWriter out) {
 
-        this.itemsLock.lock(); //Bloqueia acesso
+        this.itemsLock.lock();
 
         String line = "";
-
         try {
             this.fileItemsBReader = new BufferedReader(new FileReader(this.itemsFilePath));
 
-            while (line != null){
+            while (line != null) {
 
                 line = this.fileItemsBReader.readLine();
 
-                if (line !=null){
+                if (line != null) {
                     out.println(line);
                 }
             }
-
             out.println("####");
 
         } catch (FileNotFoundException e) {
-            System.out.println("Thread " + this.getName() + ": Erro de leitura do ficheiro de itens");
+            System.err.println("Thread " + this.getName() + ": Erro de leitura do ficheiro de itens");
             this.itemsLock.unlock();
             System.exit(5);
 
         } catch (IOException e) {
-            System.out.println("Thread " + this.getName() + ": Erro de leitura do ficheiro de itens");
+            System.err.println("Thread " + this.getName() + ": Erro de leitura do ficheiro de itens");
             this.itemsLock.unlock();
             System.exit(6);
         }
 
-        this.itemsLock.unlock(); //Desbloqueia acesso
-
+        this.itemsLock.unlock();
     }
 
-    private void sendResults(PrintWriter out){
+    private void sendResults(PrintWriter out) {
 
         String itemNameRaffle = null;
 
-        this.itemsLock.lock(); //Acesso Bloqueado
+        this.itemsLock.lock();
 
         try {
             this.fileItemsBReader = new BufferedReader(new FileReader(this.itemsFilePath));
 
             itemNameRaffle = "";
 
-            for (int i = 0 ; i <= this.itemNumberRaffle ; i++) {
+            for (int i = 0; i <= this.itemNumberRaffle; i++) {
                 itemNameRaffle = this.fileItemsBReader.readLine();
             }
 
             this.fileItemsBReader.close();
 
-
         } catch (FileNotFoundException e) {
-            System.out.println("Thread " + this.getName() + ": Ficheiro de itens não encontrado");
+            System.err.println("Thread " + this.getName() + ": Ficheiro de itens não encontrado");
+            this.itemsLock.unlock();
             System.exit(7);
 
         } catch (IOException e) {
-            System.out.println("Thread " + this.getName() + ": Erro de leitura ou de fecho do ficheiro de itens");
+            System.err.println("Thread " + this.getName() + ": Erro de leitura ou de fecho do ficheiro de itens");
+            this.itemsLock.unlock();
             System.exit(8);
         }
-        this.itemsLock.unlock(); //Acesso Desbloqueado
+        this.itemsLock.unlock();
 
         out.println("\t\t\t### Resultados do sorteio ###");
         out.println("");
@@ -230,14 +222,14 @@ public class ServerThread  extends Thread{
 
         try {
             this.fileItemsBReader = new BufferedReader(new FileReader(this.itemsFilePath));
-            this.filesRaffleBReader = new BufferedReader(new FileReader(this.raffleFilePath));
+            this.fileRaffleBReader = new BufferedReader(new FileReader(this.raffleFilePath));
 
             String itemName = "";
             String threadName = "";
-
             while (itemName != null) {
+
                 itemName = this.fileItemsBReader.readLine();
-                threadName = this.filesRaffleBReader.readLine();
+                threadName = this.fileRaffleBReader.readLine();
 
                 if (itemName != null) {
                     out.println(itemName + " - " + threadName);
@@ -246,17 +238,17 @@ public class ServerThread  extends Thread{
             out.println("####");
 
             this.fileItemsBReader.close();
-            this.filesRaffleBReader.close();
-
+            this.fileRaffleBReader.close();
 
         } catch (FileNotFoundException e) {
-            System.out.println("Thread " + this.getName() + ": Ficheiro de itens ou de sorteio não encontrado");
+            System.err.println("Thread " + this.getName() + ": Ficheiro de itens ou de sorteio não encontrado");
             this.itemsLock.unlock();
             this.raffleLock.unlock();
             System.exit(9);
 
         } catch (IOException e) {
-            System.out.println("Thread " + this.getName() + ": Erro de leitura ou fecho do ficheiro de itens ou do ficheiro de sorteio");
+            System.err.println("Thread " + this.getName() + ": Erro de leitura ou fecho do ficheiro de itens " +
+                    "ou do ficheiro de sorteio");
             this.itemsLock.unlock();
             this.raffleLock.unlock();
             System.exit(10);
